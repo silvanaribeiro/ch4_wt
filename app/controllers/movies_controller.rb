0154f194220f9b4eed05d36314ae6cf3e657a893
silvanaribeiro <smararib@uncc.edu>
2015-10-15 22:25:47 +0000

    Completed section 4.5

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,6 +12,10 @@ class MoviesController < ApplicationController
       @movie = Movie.find(id) # look up movie by unique ID
       # will render app/views/movies/show.html.haml by default
     end
+    
+    def new
+        # default: render 'new' template
+    end
 
 
 end
