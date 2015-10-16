c95118bf63928fd42db8f9cde4d02f2b774a7960
silvanaribeiro <smararib@uncc.edu>
2015-10-16 22:12:03 +0000

    Completed section 4.5

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,6 +17,11 @@ class MoviesController < ApplicationController
         # default: render 'new' template
     end
 
-
+    # in movies_controller.rb
+    def create
+      @movie = Movie.create!(params[:movie])
+      flash[:notice] = "#{@movie.title} was successfully created."
+      redirect_to movies_path
+    end
 end
 
